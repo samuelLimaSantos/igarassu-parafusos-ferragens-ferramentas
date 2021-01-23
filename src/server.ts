d18e92a0b13c0cdf2facefcb33b6abef4322ba95
Samuel Santos <samuellima280499@gmail.com>
2021-01-23 20:03:44 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'reflect-metadata';
 import './database';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
